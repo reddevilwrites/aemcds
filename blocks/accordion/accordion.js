13e2c86eb5fa5e06c0ba1e245f97368fbf31030b
reddevilwrites <raymonmaidul@gmail.com>
2025-12-16 18:27:07 +0530

    Fixing the "undefines problem

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -12,11 +12,14 @@ export default function decorate(block) {
     // 3. Create the Semantic HTML elements
     const details = document.createElement('details');
     const summaryNode = document.createElement('summary');
-    
     // 4. Move content into the new structure
-    summaryNode.append(...summary.childNodes);
-    details.append(summaryNode);
-    details.append(content);
+    if (summary) {
+        summaryNode.append(...summary.childNodes);
+        details.append(summaryNode);
+    }
+    if (content) {
+        details.append(content);
+    }
 
     // 5. Replace the original row with the new <details> element
     row.replaceWith(details);
